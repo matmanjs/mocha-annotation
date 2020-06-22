65e125c21136b7b4f82085a0262d8aa0d545b24c
linjianghe <linjianghe@tencent.com>
2020-06-22 15:36:21 +0800

    feat: 增加一个调试用的用例

---
+++ test/data/demo_01.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const fse = require('fs-extra');
+const {getParseResult} = require('../../lib');
+const testFiles = [
+  '/Users/helinjiang/gitprojects/mocha-annotation/test/data/mocha-ts-examples/one-describe-one-it.test.ts',
+  // '/Users/helinjiang/gitprojects/mocha-annotation/test/data/mocha-ts-examples/TestLoginVerifier.spec.ts',
+];
+
+const mochaTestTreeNode = getParseResult(testFiles, {isInherit: true});
+// if (needSaveTmpFiles) {
+//   // console.log(mochaTestTreeNode);
+//   fse.outputJsonSync(path.join(actualOutputPath, 'mocha-test-tree-node.json'), mochaTestTreeNode);
+// }
+
+const actualOutputPath = path.join(__dirname, 'mocha-test-tree-node.json');
+console.log(mochaTestTreeNode);
+fse.outputJsonSync(actualOutputPath, mochaTestTreeNode);
