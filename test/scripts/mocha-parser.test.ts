15eb0c9ed379290862e7b07d3b40069c33840335
helinjiang <onehlj@qq.com>
2020-07-02 22:09:20 +0800

    feat: 补充测试用例

--- test/scripts/mocha-parser.test.ts
+++ test/scripts/mocha-parser.test.ts
@@ -7,8 +7,9 @@ import {MochaTestTreeNode, TestCaseMap} from '../../src/types';
 import {findByTestId} from '../test-util';
 
 const MOCHA_EXAMPLES_PATH = path.join(__dirname, '../data/mocha-examples');
+const MOCHA_TS_EXAMPLES_PATH = path.join(__dirname, '../data/mocha-ts-examples');
 
-describe('测试 parser', function () {
+describe('测试 parser(使用 js)', function () {
   describe('getParseResult and isInherit is false', function () {
     let mochaTestTreeNode: MochaTestTreeNode;
 
@@ -192,6 +193,155 @@ describe('测试 parser', function () {
         description: 'hello,world!',
         other: 'one-describe-one-it.test.js',
         testid: 'one-describe-one-it:describe1:it1',
+      });
+      expect(treeNode.nodeInfo && treeNode.nodeInfo.describe).to.equal('1 等于 1');
+      expect(treeNode.nodeInfo && treeNode.nodeInfo.callee).to.equal('it');
+    });
+  });
+});
+
+describe('测试 parser(使用 ts)', function () {
+  describe('getParseResult and isInherit is false', function () {
+    let mochaTestTreeNode: MochaTestTreeNode;
+
+    before(function () {
+      const files = glob
+        .sync('**/*.test.ts', {
+          cwd: MOCHA_TS_EXAMPLES_PATH,
+          dot: true,
+        })
+        .map(item => path.join(MOCHA_TS_EXAMPLES_PATH, item));
+
+      mochaTestTreeNode = getParseResult(files, {isInherit: false});
+    });
+
+    it('describe 本身支持注解', function () {
+      const treeNode = findByTestId(mochaTestTreeNode, 'ts-one-describe-one-it:describe1');
+
+      expect(treeNode && treeNode.nodeInfo && treeNode.nodeInfo.callee).to.equal('describe');
+
+      expect(treeNode && treeNode.comment).to.eql({
+        author: 'matmanjs',
+        description: 'hello,ts!',
+        other: 'ts-one-describe-one-it.test.ts',
+        testid: 'ts-one-describe-one-it:describe1',
+      });
+    });
+
+    it('it 没有继承 describe 中的注解', function () {
+      const treeNode = findByTestId(mochaTestTreeNode, 'ts-one-describe-one-it:describe1:it1');
+
+      expect(treeNode && treeNode.nodeInfo && treeNode.nodeInfo.callee).to.equal('it');
+
+      expect(treeNode && treeNode.comment).to.eql({
+        testid: 'ts-one-describe-one-it:describe1:it1',
+      });
+    });
+  });
+
+  describe('getParseResult and isInherit is true', function () {
+    let mochaTestTreeNode: MochaTestTreeNode;
+
+    before(function () {
+      const files = glob
+        .sync('**/*.test.ts', {
+          cwd: MOCHA_TS_EXAMPLES_PATH,
+          dot: true,
+        })
+        .map(item => path.join(MOCHA_TS_EXAMPLES_PATH, item));
+
+      mochaTestTreeNode = getParseResult(files, {isInherit: true});
+    });
+
+    it('describe 本身支持注解', function () {
+      const treeNode = findByTestId(mochaTestTreeNode, 'ts-one-describe-one-it:describe1');
+
+      expect(treeNode && treeNode.nodeInfo && treeNode.nodeInfo.callee).to.equal('describe');
+
+      expect(treeNode && treeNode.comment).to.eql({
+        author: 'matmanjs',
+        description: 'hello,ts!',
+        other: 'ts-one-describe-one-it.test.ts',
+        testid: 'ts-one-describe-one-it:describe1',
+      });
+    });
+
+    it('it 可以继承 describe 中的注解', function () {
+      const treeNode = findByTestId(mochaTestTreeNode, 'ts-one-describe-one-it:describe1:it1');
+
+      expect(treeNode && treeNode.nodeInfo && treeNode.nodeInfo.callee).to.equal('it');
+
+      expect(treeNode && treeNode.comment).to.eql({
+        author: 'matmanjs',
+        description: 'hello,ts!',
+        other: 'ts-one-describe-one-it.test.ts',
+        testid: 'ts-one-describe-one-it:describe1:it1',
+      });
+    });
+  });
+
+  describe('getTestCaseMap and fullTitleSep is default', function () {
+    let testCaseMap: TestCaseMap;
+
+    before(function () {
+      const files = glob
+        .sync('**/*.test.ts', {
+          cwd: MOCHA_TS_EXAMPLES_PATH,
+          dot: true,
+        })
+        .map(item => path.join(MOCHA_TS_EXAMPLES_PATH, item));
+
+      testCaseMap = getTestCaseMap(getParseResult(files, {isInherit: true}));
+    });
+
+    it('验证 one-describe-one-it.test.js 一个 describe 和一个 it 1 等于 1', function () {
+      const arr = [
+        path.join(__dirname, '../data/mocha-ts-examples/ts-one-describe-one-it.test.ts'),
+        '一个 describe 和一个 it',
+        '1 等于 1',
+      ];
+      const treeNode = testCaseMap[arr.join(' ')];
+
+      expect(treeNode).to.be.a('object');
+      expect(treeNode.comment).to.eql({
+        author: 'matmanjs',
+        description: 'hello,ts!',
+        other: 'ts-one-describe-one-it.test.ts',
+        testid: 'ts-one-describe-one-it:describe1:it1',
+      });
+      expect(treeNode.nodeInfo && treeNode.nodeInfo.describe).to.equal('1 等于 1');
+      expect(treeNode.nodeInfo && treeNode.nodeInfo.callee).to.equal('it');
+    });
+  });
+
+  describe('getTestCaseMap and fullTitleSep is #', function () {
+    let testCaseMap: TestCaseMap;
+
+    before(function () {
+      const files = glob
+        .sync('**/*.test.ts', {
+          cwd: MOCHA_TS_EXAMPLES_PATH,
+          dot: true,
+        })
+        .map(item => path.join(MOCHA_TS_EXAMPLES_PATH, item));
+
+      testCaseMap = getTestCaseMap(getParseResult(files, {isInherit: true}), '#');
+    });
+
+    it('验证 one-describe-one-it.test.js#一个 describe 和一个 it#1 等于 1', function () {
+      const arr = [
+        path.join(__dirname, '../data/mocha-ts-examples/ts-one-describe-one-it.test.ts'),
+        '一个 describe 和一个 it',
+        '1 等于 1',
+      ];
+      const treeNode = testCaseMap[arr.join('#')];
+
+      expect(treeNode).to.be.a('object');
+      expect(treeNode.comment).to.eql({
+        author: 'matmanjs',
+        description: 'hello,ts!',
+        other: 'ts-one-describe-one-it.test.ts',
+        testid: 'ts-one-describe-one-it:describe1:it1',
       });
       expect(treeNode.nodeInfo && treeNode.nodeInfo.describe).to.equal('1 等于 1');
       expect(treeNode.nodeInfo && treeNode.nodeInfo.callee).to.equal('it');
